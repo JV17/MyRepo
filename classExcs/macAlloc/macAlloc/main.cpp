dbe4b1508b8a87bcc43b0834e20b7b517324ae6b
JV17 <jevalbuenasanchez@myseneca.ca>
2013-11-07 14:02:36 -0500

    Initial Commit

---
+++ classExcs/macAlloc/macAlloc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  macAlloc
+//
+//  Created by Jorge Valbuena on 11/7/2013.
+//  Copyright (c) 2013 Jorge Valbuena. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
