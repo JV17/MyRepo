fd5b5a146df90588aa38aa702b76f8af03ae94c6
Jorge Valbuena <jevalbuenasanchez@myseneca.ca>
2014-04-28 16:26:51 -0400

    Adding TypeTutor program..
    Helps the user to see which key is pressing without looking at the
    keyboard.

--- Java/TypeTutor/src/typeTutor.java
+++ Java/TypeTutor/src/typeTutor.java
@@ -3,7 +3,7 @@ Name: Jorge E. Valbuena S.
 Date: March 17, 2014
 Assignment #: Assignment 3
 General description of what your code does:
-	This code create a virtual keyboard and helps the user to learn how to type without 
+	This code create a virtual keyboard and helps the user to learn how to type without
 	looking at the keyboard
 General description of the variables used in your code:
 	firstRow hold the string for the the first row of button for the keyboard
@@ -16,12 +16,11 @@ General description of the variables used in your code:
     specialChars hold the special characters
     keycode to hold the virtual keycode values
 Expected good results:
-	This code will change the background button of the keyboard pressed in the screen and 
+	This code will change the background button of the keyboard pressed in the screen and
 	display all characters typed by the user.
 Anticipated bad results:
 	This program allows user interaction, which may cause to throw some exceptions that are not supported
 ******************************************************************************/
-package typetutor;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -45,22 +44,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-public class TypeTutor extends JFrame implements KeyListener {
-
+public class typeTutor extends JFrame implements KeyListener {
+    
     private static final long serialVersionUID = 1L;
-	
+    
     // creating map for buttons
-    Map<Integer, JButton> map = new HashMap<Integer, JButton>();
+    Map<Integer, JButton> map = new HashMap<>();
 
     // Individual keyboard rows
     String firstRow[] = { "~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "fill",
-            			  "BackSpace" };
+                          "BackSpace" };
     String secondRow[] = { "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\" };
     String thirdRow[] = { "Caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "fill",
-            			  "fill", "Enter" };
+            	          "fill", "Enter" };
     String fourthRow[] = { "Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "?", "blank", "^" };
     String fifthRow[] = { "blank", "blank", "fill", "fill", "fill", "fill", "fill", "fill", "fill",
-            			  "fill", "", "<", "v", ">" };
+                          "fill", "", "<", "v", ">" };
     String strText = "";
     // all keys without shift key press
     String noShift = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
@@ -79,30 +78,148 @@ public class TypeTutor extends JFrame implements KeyListener {
     // default color of the button to be repainted when key released
     Color cc = new JButton().getBackground();
 
+    
     // Driver main method to start the application
     public static void main(String[] args) {
+        
         // launch typing tutor
-    	TypeTutor a = new TypeTutor();
-        a.setVisible(true);
-    }
+    	typeTutor a = new typeTutor();
+      
+    }// end of main 
 
+    
     // No argument constructor to create frame
-    public TypeTutor() {
+    public typeTutor() {
+ 
     	// program label
         super("Type Tutor - My JAC444");
         
-        /* Creating gradient background
-        JPanel contentPane = new JPanel() {
-			private static final long serialVersionUID = 1L;
+        // Calling gradientBg method
+        gradientBg();
+        
+        // calling gradientBg method
+        init();
+        
+    }// end of Constructor
 
-			@Override
+    
+    // init function
+    public final void init(){
+        
+        // set the info label on top
+        JLabel info = new JLabel(
+                "<html>&nbsp;&nbsp;Type some text using your keyboard.The keys "
+                 + "you press will be highlighted and text will be displayed."
+                 + "<br> &nbsp;&nbsp;Note : Clicking the buttons with your "
+                 + "mouse will not perform any action. <br><br> </html>");
+        
+        // set the bold font for info
+        info.setFont(new Font("Verdana", Font.BOLD, 12));
+
+        // set the layout and place component in place and pack it
+        setLayout(new BorderLayout());
+
+        // Various panel for the layout
+        JPanel jpNorth = new JPanel();
+        JPanel jpWest = new JPanel();
+        JPanel jpEast = new JPanel();
+        JPanel jpCenter = new JPanel();
+        JPanel jpKeyboard = new JPanel(new GridBagLayout());
+        JPanel jpNote = new JPanel();
+
+        // adding Panels
+        add(jpNorth, BorderLayout.NORTH);
+        add(jpNote);
+        
+        // adding Panels
+        add(jpWest, BorderLayout.WEST);
+        add(jpEast, BorderLayout.EAST);
+        
+        // adding Panels
+        add(jpCenter, BorderLayout.CENTER);
+        add(jpKeyboard, BorderLayout.SOUTH);
+
+        // setting layout and adding BorderLayout
+        jpNorth.setLayout(new BorderLayout());
+        jpNorth.add(info, BorderLayout.SOUTH);
+
+        // setting layout and adding BorderLayout
+        jpCenter.setLayout(new BorderLayout());
+
+        //jpCenter.add(text, BorderLayout.WEST);
+        jpCenter.add(text, BorderLayout.CENTER);
+
+        // setting preferredsize
+        jpCenter.setPreferredSize(new Dimension(200, 140));
+
+        // creating buttons
+        first = new JButton[firstRow.length];
+        second = new JButton[secondRow.length];
+        third = new JButton[thirdRow.length];
+        fourth = new JButton[fourthRow.length];
+        fifth = new JButton[fifthRow.length];
+
+        // adding buttons
+        addKeys(jpKeyboard, 0, firstRow, first);
+        addKeys(jpKeyboard, 1, secondRow, second);
+        addKeys(jpKeyboard, 2, thirdRow, third);
+        addKeys(jpKeyboard, 3, fourthRow, fourth);
+        addKeys(jpKeyboard, 4, fifthRow, fifth);
+
+        // setting preferredSize
+        jpKeyboard.setPreferredSize(new Dimension(300, 140));
+        
+        // fixing background and panels overflow
+        info.setOpaque(false);
+        jpNote.setOpaque(false);
+        jpWest.setOpaque(false);
+        jpEast.setOpaque(false);
+        jpNorth.setOpaque(false);
+        jpCenter.setOpaque(false);
+        jpKeyboard.setOpaque(false);
+
+        // add listeners
+        text.addKeyListener(this);
+
+        // setting default close operation
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // set non re-sizable
+        this.setResizable(false);
+
+        // set size of the content pane ie frame
+        this.getContentPane().setPreferredSize(new Dimension(700, 310));
+
+        // putting everything together
+        pack();
+        
+        // brings this Window to the front and may make it the focused Window
+        this.toFront();
+
+        // Window appears center
+        this.setLocationRelativeTo(null);
+        
+         // setting visible
+        this.setVisible(true);
+    
+    }// end of init 
+    
+    
+    // background gradient function
+    public final void gradientBg(){
+        
+        // Creating gradient background
+        JPanel contentPane = new JPanel() {
+	    private static final long serialVersionUID = 1L;
+            
+            @Override
             protected void paintComponent(Graphics grphcs) {
                 Graphics2D g2d = (Graphics2D) grphcs;
                 Dimension d = this.getSize();
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                         RenderingHints.VALUE_ANTIALIAS_ON);
 
-                GradientPaint gp = new GradientPaint(0, 0, getBackground().brighter().brighter(),
+                GradientPaint gp = new GradientPaint(0, 0, getBackground().brighter(),
                         0, d.height, getBackground().darker().darker());
 
                 g2d.setPaint(gp);
@@ -113,125 +230,50 @@ public class TypeTutor extends JFrame implements KeyListener {
         };
         contentPane.setOpaque(false);
         setContentPane(contentPane);
-		*/
-
-        // set the info label on top
-        JLabel info = new JLabel(
-                "<html>&nbsp;&nbsp;Type some text using your keyboard.The keys you press will be highlighted and text will be displayed.<br> &nbsp;&nbsp;Note : Clicking the buttons with your mouse will not perform any action. <br><br> </html>");
-        // set the bold font for info
-        info.setFont(new Font("Verdana", Font.BOLD, 12));
-
-        // set the layout and place component in place and pack it
-        setLayout(new BorderLayout());
-
-        // Various panel for the layout
-        JPanel jpNorth = new JPanel();
-        JPanel jpCenter = new JPanel();
-        JPanel jpKeyboard = new JPanel(new GridBagLayout());
-        JPanel jpNote = new JPanel();
         
-        // adding Panels
-        add(jpNorth, BorderLayout.NORTH);
-        add(jpNote);
-        
-        // adding Panels
-        add(jpCenter, BorderLayout.CENTER);
-        add(jpKeyboard, BorderLayout.SOUTH);
-        
-        // setting layout and adding BorderLayout
-        jpNorth.setLayout(new BorderLayout());
-        jpNorth.add(info, BorderLayout.WEST);
-        jpNorth.add(info, BorderLayout.SOUTH);
-
-        // setting layout and adding BorderLayout
-        jpCenter.setLayout(new BorderLayout());
-        jpCenter.add(text, BorderLayout.WEST);
-        jpCenter.add(text, BorderLayout.CENTER);
-        
-        // creating buttons
-        first = new JButton[firstRow.length];
-        second = new JButton[secondRow.length];
-        third = new JButton[thirdRow.length];
-        fourth = new JButton[fourthRow.length];
-        fifth = new JButton[fifthRow.length];
-        
-        // adding buttons
-        addKeys(jpKeyboard, 0, firstRow, first);
-        addKeys(jpKeyboard, 1, secondRow, second);
-        addKeys(jpKeyboard, 2, thirdRow, third);
-        addKeys(jpKeyboard, 3, fourthRow, fourth);
-        addKeys(jpKeyboard, 4, fifthRow, fifth);
-        
-        // setting preferredSize
-        jpKeyboard.setPreferredSize(new Dimension(800, 160));
-
-        /*
-        // fixing background and panels overflow
-        info.setOpaque(false);
-        jpNote.setOpaque(false);
-        jpNorth.setOpaque(false);
-        jpCenter.setOpaque(false);
-        jpKeyboard.setOpaque(false);
-		*/
-        
-        // add listeners
-        text.addKeyListener(this);
-
-        // setting default close operation
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        // set non re-sizable
-        this.setResizable(false);
-        
-        // set size of the content pane ie frame
-        this.getContentPane().setPreferredSize(new Dimension(1100, 350));
-        
-        // putting everything together
-        pack();
-        
-        // brings this Window to the front and may make it the focused Window
-        this.toFront(); 
-        
-        // Window appears center
-        this.setLocationRelativeTo(null); 
-
-    }// end of Constructor
+    }// end of gradientBg
     
     
     // Invoked when a key has been pressed.
     // @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
     @Override
     public void keyPressed(KeyEvent evt) {
+        
         keycode = evt.getKeyCode();
         strText = String.format("%s", KeyEvent.getKeyText(evt.getKeyCode()));
-        
+
         JButton btn = map.get(keycode);
         if (btn != null) {
-             map.get(keycode).setBackground(Color.BLUE);
+             map.get(keycode).setBackground(new Color(135,206,235));
         }
+        
     }// end of key pressed
 
-    
+
     // Invoked when a key has been released
     @Override
     public void keyReleased(KeyEvent evt) {
+        
         keycode = evt.getKeyCode();
         strText = String.format("%s", KeyEvent.getKeyText(evt.getKeyCode()));
-        
+
         JButton btn = map.get(keycode);
         if (btn != null) {
              map.get(keycode).setBackground(Color.WHITE);
         }
+        
     }// end of keyReleased
-    
-    
+
+
     // Invoked when a key has been typed
     @Override
     public void keyTyped(KeyEvent evt) {
+      
        strText = String.format("%s", evt.getKeyChar());
+       
     }// end of key typed
-    
-    
+
+
     // adds the buttons to create screen keyboard
     protected final void addKeys(JPanel parent, int row, String[] keys, JButton[] buttons) {
 
@@ -257,47 +299,50 @@ public class TypeTutor extends JFrame implements KeyListener {
                 gap = 0;
 
                 btn.setBackground(Color.WHITE);
-                map.put(getKeyCode(key), btn); 
+                map.put(getKeyCode(key), btn);
             }
         }
     }// end of addKeys
-    
-    
+
+
     // checks for virtual key code events
     private int getKeyCode(String key) {
-        if (key.equals("BackSpace")) {
-            return KeyEvent.VK_BACK_SPACE;
-        } else if (key.equals("Tab")) {
-            return KeyEvent.VK_TAB;
-        } else if (key.equals("Caps")) {
-            return KeyEvent.VK_CAPS_LOCK;
-        } else if (key.equals("Enter")) {
-            return KeyEvent.VK_ENTER;
-        } else if (key.equals("Shift")) {
-            return KeyEvent.VK_SHIFT;
-        } else if (key.equals("")) {
-            return KeyEvent.VK_SPACE;
-        } else if (key.equals("+")) {
-            return KeyEvent.VK_EQUALS;
-        }else if (key.equals(":")) {
-            return KeyEvent.VK_SEMICOLON;
-        }else if (key.equals("\"")) {
-            return KeyEvent.VK_QUOTE;
-        }else if (key.equals("?")) {
-            return KeyEvent.VK_SLASH;
-        }else if (key.equals("~")) {
-            return KeyEvent.VK_BACK_QUOTE;
-        } else if (key.equals("^")) {
-            return KeyEvent.VK_UP;
-        } else if (key.equals("v")) {
-            return KeyEvent.VK_DOWN;
-        } else if (key.equals("<")) {
-            return KeyEvent.VK_LEFT;
-        } else if (key.equals(">")) {
-            return KeyEvent.VK_RIGHT;
-        } else {
-            return (int) key.charAt(0);
-        }
+        
+        switch (key) {
+            case "BackSpace":
+                return KeyEvent.VK_BACK_SPACE;
+            case "Tab":
+                return KeyEvent.VK_TAB;
+            case "Caps":
+                return KeyEvent.VK_CAPS_LOCK;
+            case "Enter":
+                return KeyEvent.VK_ENTER;
+            case "Shift":
+                return KeyEvent.VK_SHIFT;
+            case "":
+                return KeyEvent.VK_SPACE;
+            case "+":
+                return KeyEvent.VK_EQUALS;
+            case ":":
+                return KeyEvent.VK_SEMICOLON;
+            case "\"":
+                return KeyEvent.VK_QUOTE;
+            case "?":
+                return KeyEvent.VK_SLASH;
+            case "~":
+                return KeyEvent.VK_BACK_QUOTE;
+            case "^":
+                return KeyEvent.VK_UP;
+            case "v":
+                return KeyEvent.VK_DOWN;
+            case "<":
+                return KeyEvent.VK_LEFT;
+            case ">":
+                return KeyEvent.VK_RIGHT;
+            default:
+                return (int) key.charAt(0);       
+        }// end of switch
+        
     }// end of getKeyCode
-
-}// end of class
+    
+}// end of typeTutor class
